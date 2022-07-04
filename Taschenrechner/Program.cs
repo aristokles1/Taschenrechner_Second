c0a390535d591544ef1170e2ee7dfb9b37d32ae5
Hermann Bernard <hermann.bernard@outlook.de>
2022-07-04 06:17:04 +0200

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
+            Console.Write("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandel Text in Gleikommazahlen
+            double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
+            double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
+
+            // Berechnung ausführen
+            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe
+            Console.WriteLine("Die Summe ist: {0}", summe);
+            Console.ReadLine();
+        }
+    }
+}
